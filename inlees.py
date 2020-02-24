2376087f073cb02df1d9b75f94080f54f4133058
ruben kindt <rubenkindt@live.be>
2020-02-24 10:46:13 +0100

    added toledo docs

--- inlees.py
+++ inlees.py
@@ -1,1 +1,20 @@
-inlees():
+from readFile import ReadFile
+
+
+class inlees(location):
+    file=None
+    
+    def __init__(location):
+        self.location=location
+        self.file=open((self.location+"txt"),"r+")    
+                   
+       
+    
+    def lees():
+        line=self.file.readline()
+        
+        print("contens of line",line)
+        nr=nr.split(': ')
+        print('contens of line[0]',nr[0])
+        print('contens of line[1]',nr[1])
+        
