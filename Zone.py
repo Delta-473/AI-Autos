2bacfcabcb6bb5400d25d059dea8f675586079dc
Jeroen14 <r0631103@student.thomasmore.be>
2020-02-24 11:39:38 +0100

    Reservatie init

--- Zone.py
+++ Zone.py
@@ -2,11 +2,11 @@
 #		id String
 #		zone aanliggende [] Strings
 
-
+from typing import Set
 class Zone:
 	def _init_(self, id, neighbours):
 		self.id= id
-        self.neighbours: [str] = set(neighbours.split(","))	# To intialize a set with values, you can pass in a list to set()
+        self.neighbours: Set[str] = set(neighbours.split(","))	# To intialize a set with values, you can pass in a list to set()
 		
 	def __str__(self):
         return 'Zone <id: '+self.id+', neighbours: '+(self.neighbours)+''
