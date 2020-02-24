bca32e604ce2b11d9b65adc92fafad92693a07ec
Jeroen14 <r0631103@student.thomasmore.be>
2020-02-24 11:08:54 +0100

    zone edited

--- Zone.py
+++ Zone.py
@@ -1,4 +1,12 @@
-	Zone
-		id String
-		zone aanliggende [] Strings
+#	Zone
+#		id String
+#		zone aanliggende [] Strings
 
+
+class Zone:
+	def _init_(self, id, neighbours):
+		self.id= id
+        self.neighbours: [str] = set(neighbours.split(","))	# To intialize a set with values, you can pass in a list to set()
+		
+		
+	
