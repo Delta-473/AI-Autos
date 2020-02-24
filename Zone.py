d3e327ac608f6ba68a217bd5cfb7dca732cf5936
Ruben Kindt <46008470+rubenkindt@users.noreply.github.com>
2020-02-24 11:59:11 +0100

    Update Zone.py

--- Zone.py
+++ Zone.py
@@ -1,5 +1,5 @@
 
-<<<<<<< HEAD
+
 class Zone():
     zoneId=""
     neighbours=""
@@ -10,15 +10,3 @@ class Zone():
        
     def __str__(self):
         print('Zone <id: '+self.zoneId+', neighbours: '+(self.neighbours)+'')
-=======
-from typing import Set
-class Zone:
-	def _init_(self, id, neighbours):
-		self.id= id
-        self.neighbours: Set[str] = set(neighbours.split(","))	# To intialize a set with values, you can pass in a list to set()
-		
-	def __str__(self):
-        return 'Zone <id: '+self.id+', neighbours: '+(self.neighbours)+''
->>>>>>> fe742b9a21af34c7456dc9d8863609e5c9865172
-		
-	
