081ba15f15ec6821a5a1d820523d6dad4825c30a
Jeroen14 <r0631103@student.thomasmore.be>
2020-02-24 11:41:52 +0100

    Reservatie

--- Reservatie.py
+++ Reservatie.py
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-	# reservatie
-		# id String
-		# dag int
-		# zone-id String
-		# start int
-		# duur int
-		# voertuigen[] Strings
-		# pentalty1 int
-		# pentalty2 int
-from typing import List
-from AI-Autos.Zone import Zone
-
-class Reservatie 		
-	def __init__(self, id, zone, dag, start, duur, voertuigen, penalty1, penalty2,)
-		self.id = str(id)
-		self.zone= Zone(zone)
-		self.dag = int(dag)
-		self.start= int(start)
-		self.duur= int(duur)
-		self.voertuigen= List[str]=voertuigen.split(",") 
-		self.penalty1=int(penalty1
-		self.penalty2= int(penalty2)
-	
-=======
 class Reservatie():
     resId=""
     zoneId=""
