3ac3cd8c454319d052cb713241072a067fae74d2
ruben kindt <rubenkindt@live.be>
2020-02-24 10:49:26 +0100

    added toledo docs

--- inlees.py
+++ inlees.py
@@ -6,8 +6,8 @@ class inlees(location):
     
     def __init__(location):
         self.location=location
-        self.file=open((self.location+"txt"),"r+")    
-                   
+        self.file=open(("100_5_14_25.csv"),"r+")
+        
        
     
     def lees():
