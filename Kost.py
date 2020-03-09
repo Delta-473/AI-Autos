f5becd6a1c0c4489d57691b585b551c4c54934cb
ruben kindt <rubenkindt@live.be>
2020-03-09 11:14:37 +0100

    l

--- Kost.py
+++ Kost.py
@@ -1,3 +1,9 @@
 
 
-    def kost(self):
+    def kost(self,reservaties):
+        som=0
+        for res in reservaties:
+            if(res.isToegewezen() and res.getzone() == res.toegewezenVoertuig().getzone()):
+                sum+=res.getp2()
+            else if(not (res.isToegewezen())):
+                sum+=res.getp1()
--- models/Voertuig.py
+++ models/Voertuig.py
@@ -11,3 +11,6 @@ class Voertuig():
 
     def __str__(self):
         return f"Voertuig ID: {self.id} en zone ID: {self.zoneId}"
+
+    def getZone(self):
+        return self.zoneID
