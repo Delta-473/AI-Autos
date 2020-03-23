7347b067129527b33125863d1f7dbd774ba466d0
Ruben Kindt <rubenkindt@live.be>
2020-03-23 10:10:43 +0100

    solved merge conf

--- models/Voertuig.py
+++ models/Voertuig.py
@@ -5,18 +5,12 @@ class Voertuig():
     id=""
     zoneId=""
 
-<<<<<<< HEAD
     def __init__(self,id):
         self.id=id
         #print("voertuig made",self.id)
 
-    #def getID(self):
-     #   return self.id
-=======
-    def __init__(self, id):
-        self.id = id
-        print("voertuig made", self.id)
->>>>>>> origin/master
+    def getID(self):
+        return self.id
 
     def __str__(self):
         return f"Voertuig ID: {self.id} en zone ID: {self.zoneId}"
