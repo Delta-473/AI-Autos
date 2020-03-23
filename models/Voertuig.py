f36be0c612a474f263ae29c1fe293b5d4c0aa3c6
inwinhawk <jv.thomasmore@gmail.com>
2020-03-23 09:10:04 +0100

    verwijderen conflict van Voertuig.py

--- models/Voertuig.py
+++ models/Voertuig.py
@@ -5,18 +5,9 @@ class Voertuig():
     id=""
     zoneId=""
 
-<<<<<<<<< Temporary merge branch 1
     def __init__(self,id):
         self.id=id
-        #print("voertuig made",self.id)
-
-    #def getID(self):
-     #   return self.id
-=========
-    def __init__(self, id):
-        self.id = id
-        print("voertuig made", self.id)
->>>>>>>>> Temporary merge branch 2
+        print("voertuig made",self.id)
 
     def __str__(self):
         return f"Voertuig ID: {self.id} en zone ID: {self.zoneId}"
