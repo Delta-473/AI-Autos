4876d6994f5cd20b22d55ebf9aa4011dfab2afaa
Ruben Kindt <46008470+rubenkindt@users.noreply.github.com>
2020-05-04 14:58:00 +0200

    added results and final paper

---
+++ solutions/Toledo opgaves/validator_first_batch.bat
@@ -0,0 +1,21 @@
+java -jar validator.jar ./100_5_14_25.csv ./solution1_100_5_14_25.csv
+java -jar validator.jar ./100_5_14_25.csv ./solution2_100_5_14_25.csv
+java -jar validator.jar ./100_5_14_25.csv ./solution3_100_5_14_25.csv
+
+java -jar validator.jar ./100_5_19_25.csv ./solution1_100_5_19_25.csv
+java -jar validator.jar ./100_5_19_25.csv ./solution2_100_5_19_25.csv
+java -jar validator.jar ./100_5_19_25.csv ./solution3_100_5_19_25.csv
+
+java -jar validator.jar ./210_5_33_25.csv ./solution1_210_5_33_25.csv
+java -jar validator.jar ./210_5_33_25.csv ./solution2_210_5_33_25.csv
+java -jar validator.jar ./210_5_33_25.csv ./solution3_210_5_33_25.csv
+
+java -jar validator.jar ./210_5_44_25.csv ./solution1_210_5_44_25.csv
+java -jar validator.jar ./210_5_44_25.csv ./solution2_210_5_44_25.csv
+java -jar validator.jar ./210_5_44_25.csv ./solution3_210_5_44_25.csv
+
+java -jar validator.jar ./360_5_71_25.csv ./solution1_360_5_71_25.csv
+java -jar validator.jar ./360_5_71_25.csv ./solution2_360_5_71_25.csv
+java -jar validator.jar ./360_5_71_25.csv ./solution3_360_5_71_25.csv
+
+pause
---
+++ solutions/instances/validator_sec_batch.bat
@@ -0,0 +1,24 @@
+java -jar validator.jar ./instance01.csv ./solution1_instance01.csv
+java -jar validator.jar ./instance01.csv ./solution2_instance01.csv
+java -jar validator.jar ./instance01.csv ./solution3_instance01.csv
+
+java -jar validator.jar ./instance02.csv ./solution1_instance02.csv
+java -jar validator.jar ./instance02.csv ./solution2_instance02.csv
+java -jar validator.jar ./instance02.csv ./solution3_instance02.csv
+
+java -jar validator.jar ./instance03.csv ./solution1_instance03.csv
+java -jar validator.jar ./instance03.csv ./solution2_instance03.csv
+java -jar validator.jar ./instance03.csv ./solution3_instance03.csv
+
+java -jar validator.jar ./instance04.csv ./solution1_instance04.csv
+java -jar validator.jar ./instance04.csv ./solution2_instance04.csv
+java -jar validator.jar ./instance04.csv ./solution3_instance04.csv
+
+java -jar validator.jar ./instance05.csv ./solution1_instance05.csv
+java -jar validator.jar ./instance05.csv ./solution2_instance05.csv
+java -jar validator.jar ./instance05.csv ./solution3_instance05.csv
+
+java -jar validator.jar ./instance06.csv ./solution1_instance06.csv
+java -jar validator.jar ./instance06.csv ./solution2_instance06.csv
+java -jar validator.jar ./instance06.csv ./solution3_instance06.csv
+pause
